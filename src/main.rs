a0a78326096bb5e200cbb773572287368ef3fc4b
astafiev <astafiev@poggiplaydev.com>
2024-01-16 21:21:16 +0400

    implement go to inside dir and back

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,22 @@ struct Point {
     y: u16
 }
 
+#[derive(Debug)]
+struct File {
+    name: String,
+    is_dir: bool,
+    path: PathBuf
+}
+
 struct Explorer {
     stdout: RawTerminal<Stdout>,
     folder_fg: color::Fg<color::Blue>,
     scroll_y: u16,
     cursor: Point,
     list_length: usize,
-    dirs: Option<Vec<String>>,
-    files: Option<Vec<String>>,
+    entries: Vec<File>,
     outputed_count: u16,
-    path: String
+    path: PathBuf,
 }
 
 impl Explorer {
@@ -30,13 +36,12 @@ impl Explorer {
         Explorer {
             stdout: io::stdout().into_raw_mode().unwrap(),
             folder_fg: color::Fg(color::Blue),
-            cursor: Point { x: 1, y: 0 },
+            cursor: Point { x: 1, y: 1 },
             list_length: 0,
-            dirs: None,
-            files: None,
-            path: "/home/artem/".to_string(),
+            entries: Vec::new(),
+            path: PathBuf::from("./test_dir"),
             outputed_count: 0,
-            scroll_y: 0
+            scroll_y: 0,
         }
     }
 
@@ -47,17 +52,25 @@ impl Explorer {
         let stdin = io::stdin();
         for c in stdin.keys() {
             match c.unwrap() {
-                Key::Char('q') => break,
-                Key::Char('k') => self.dec_cursor_y(),
-                Key::Char('j') => self.inc_cursor_y(),
-                Key::Char('g') => self.go_up(),
-                Key::Char('d') => self.go_down(),
+                Key::Char('q')  => break,
+                Key::Char('k')  => self.dec_cursor_y(),
+                Key::Char('j')  => self.inc_cursor_y(),
+                Key::Char('g')  => self.go_up(),
+                Key::Char('d')  => self.go_down(),
+                Key::Char('\n') => {
+                    self.go_inside_dir();
+                    self.read_dir();
+                },
+                Key::Backspace  => {
+                    self.go_back();
+                    self.read_dir();
+                },
                 _ => ()
             }
 
             self.update_entry_list();
 
-            self.stdout.flush().unwrap()
+            self.stdout.flush().unwrap();
         }
     }
 
@@ -66,24 +79,22 @@ impl Explorer {
 
         self.list_length = 0;
 
-        let mut files = Vec::new();
-        let mut dirs = Vec::new();
+        self.scroll_y = 0;
+
+        self.entries = Vec::new();
+
         for entry in fs::read_dir(&self.path).unwrap() {
             let e = entry.unwrap();
             let file_name = e.file_name().to_string_lossy().into_owned();
-            if e.path().is_dir() {
-                dirs.push(file_name);
-            } else {
-                files.push(file_name);
-            }
+            self.entries.push(File {
+                name: file_name,
+                is_dir: e.path().is_dir(),
+                path: e.path()
+            });
             self.list_length += 1;
         }
 
-        dirs.sort();
-        files.sort();
-
-        self.dirs = Some(dirs);
-        self.files = Some(files);
+        self.entries.sort_by_key(|a| !a.is_dir);
     }
 
     fn update_entry_list(&mut self) {
@@ -99,24 +110,20 @@ impl Explorer {
                  color::Bg(color::Reset),
         );
 
-        let dirs_len = self.dirs.as_ref().unwrap().len();
-        let files_len = self.files.as_ref().unwrap().len();
-        let entries_len = dirs_len + files_len;
-
         self.outputed_count = 0;
 
-        for i in self.scroll_y as usize..entries_len {
+        for i in self.scroll_y as usize..self.entries.len() {
             if i as i32 >= size_y as i32 + self.scroll_y as i32 - 2 {
                 break;
             }
-            if i < entries_len - files_len {
+            let curr_entry = self.entries.get(i).unwrap();
+            if curr_entry.is_dir {
                 println!("{}{}{}\r",
-                         self.folder_fg,
-                         self.dirs.as_ref().unwrap().get(i).unwrap(),
-                         color::Fg(color::Reset));
+                            self.folder_fg,
+                            curr_entry.name,
+                            color::Fg(color::Reset));
             } else {
-                // println!("{} of {} | {}\r", i, files_len, entries_len);
-                println!("{}\r", self.files.as_ref().unwrap().get(i % files_len).unwrap());
+                println!("{}\r", curr_entry.name);
             }
             self.outputed_count += 1;
         }
@@ -144,7 +151,9 @@ impl Explorer {
         if self.cursor.y < self.outputed_count {
             self.cursor.y += 1;
         } else {
-            self.scroll_y += 1;
+            if self.scroll_y < self.list_length as u16 % termion::terminal_size().unwrap().1 + 2  {
+                self.scroll_y += 1;
+            }
         }
         println!(
             "{}",
@@ -160,6 +169,30 @@ impl Explorer {
             "{}",
             termion::cursor::Goto(self.cursor.x, self.cursor.y)
         );
+    }
+
+    fn go_inside_dir(&mut self) {
+        let curr_file = self.entries.get(self.cursor.y as usize - 1).unwrap();
+        if curr_file.is_dir {
+            self.path = curr_file.path.clone();
+        }
+    }
+
+    fn go_back(&mut self) {
+        let path = self.path.to_str().unwrap();
+        let mut slash_index = 0;
+
+        for i in 0..self.path.to_str().unwrap().len() {
+            if path.as_bytes()[i] as char == '/' {
+                slash_index = i;
+            }
+        }
+
+        self.path = PathBuf::from(path[..slash_index].to_owned())
+    }
+
+    fn get_cursor_filename(&self) {
+
     }
 
     fn go_down(&mut self) {
