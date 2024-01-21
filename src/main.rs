2ad5df423f163ade09cf2519a42d471208218ec5
astafiev <astafiev@poggiplaydev.com>
2024-01-21 12:43:04 +0400

    add rename ability

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::{self, Stdout, Write}, path::PathBuf};
+use std::{fs, io::{self, Stdout, Write}, path::PathBuf, collections::HashMap, borrow::Borrow};
 use termion::{raw::{IntoRawMode, RawTerminal}, color::{self}};
 use termion::input::TermRead;
 use termion::event::Key;
@@ -16,7 +16,6 @@ struct Point {
 #[derive(Debug)]
 struct File {
     name: String,
-    is_dir: bool,
     path: PathBuf
 }
 
@@ -52,12 +51,16 @@ impl Explorer {
         let stdin = io::stdin();
         for c in stdin.keys() {
             match c.unwrap() {
-                Key::Char('q')  => break,
+                Key::Char('q')  => {
+                    println!("{}{}\r", termion::cursor::Goto(1, 1), termion::clear::All);
+                    break
+                },
                 Key::Char('k')  => self.dec_cursor_y(),
                 Key::Char('j')  => self.inc_cursor_y(),
                 Key::Char('g')  => self.go_up(),
                 Key::Char('d')  => self.go_down(),
                 Key::Char('\n') => self.go_inside_dir(),
+                Key::Char('r')  => self.rename_file(),
                 Key::Backspace  => self.go_back(),
                 _ => ()
             }
@@ -83,13 +86,12 @@ impl Explorer {
             let file_name = e.file_name().to_string_lossy().into_owned();
             self.entries.push(File {
                 name: file_name,
-                is_dir: e.path().is_dir(),
                 path: e.path()
             });
             self.list_length += 1;
         }
 
-        self.entries.sort_by_key(|a| !a.is_dir);
+        self.entries.sort_by_key(|a| !a.path.is_dir());
     }
 
     fn print_entries_list(&mut self) {
@@ -112,11 +114,11 @@ impl Explorer {
                 break;
             }
             let curr_entry = self.entries.get(i).unwrap();
-            if curr_entry.is_dir {
+            if curr_entry.path.is_dir() {
                 println!("{}{}{}\r",
-                            self.folder_fg,
-                            curr_entry.name,
-                            color::Fg(color::Reset));
+                         self.folder_fg,
+                         curr_entry.name,
+                         color::Fg(color::Reset));
             } else {
                 println!("{}\r", curr_entry.name);
             }
@@ -146,8 +148,8 @@ impl Explorer {
         if self.cursor.y < self.outputed_count {
             self.cursor.y += 1;
         } else {
-            if  (self.cursor.y > termion::terminal_size().unwrap().1 - 4) &&
-                (self.scroll_y < self.list_length as u16 % termion::terminal_size().unwrap().1 + 2)
+            if (self.cursor.y > termion::terminal_size().unwrap().1 - 4) &&
+               (self.scroll_y < self.list_length as u16 % termion::terminal_size().unwrap().1 + 2)
             {
                 self.scroll_y += 1;
             }
@@ -169,8 +171,8 @@ impl Explorer {
     }
 
     fn go_inside_dir(&mut self) {
-        let curr_file = self.entries.get(self.cursor.y as usize - 1).unwrap();
-        if curr_file.is_dir {
+        let curr_file = self.get_curr_file();
+        if curr_file.path.is_dir() {
             self.path = fs::canonicalize(curr_file.path.clone()).unwrap();
             self.read_dir();
         }
@@ -183,11 +185,133 @@ impl Explorer {
         }
     }
 
+    fn rename_file(&mut self) {
+        let mut input_window = InputWindow::new();
+
+        input_window.set_content(self.get_curr_filename());
+        input_window.render();
+
+        for key in std::io::stdin().keys() {
+            match key.unwrap() {
+                Key::Backspace => input_window.remove_last_char(),
+                Key::Ctrl('c') => return (),
+                Key::Char('\n') => {
+                    if input_window.content.len() > 0 {
+                        break;
+                    } else {
+                        continue;
+                    }
+                },
+                Key::Char(c) => input_window.add_char(c),
+                _ => ()
+            }
+
+            self.print_entries_list();
+            input_window.render();
+        }
+
+        let curr_file = self.get_curr_file();
+        let before_rename = curr_file.path.as_path().to_owned();
+        curr_file.name = input_window.content.clone();
+        curr_file.path.set_file_name(input_window.content);
+        fs::rename(before_rename, curr_file.path.as_path()).unwrap();
+    }
+
     fn go_down(&mut self) {
         self.cursor.y = self.outputed_count as u16;
         println!(
             "{}",
             termion::cursor::Goto(self.cursor.x, self.cursor.y)
         );
+    }
+
+    fn get_curr_file(&mut self) -> &mut File {
+        self.entries.get_mut(self.cursor.y as usize - 1).unwrap()
+    }
+
+    fn get_curr_filename(&mut self) -> String {
+        self.entries.get(self.cursor.y as usize - 1).unwrap().name.to_string()
+    }
+}
+
+struct InputWindow {
+    input: Option<String>,
+    size: (u16, u16),
+    pos: (u16, u16),
+    title: String,
+    scroll_x: u16,
+    content: String,
+}
+
+impl InputWindow {
+    fn new() -> InputWindow {
+        let size = termion::terminal_size().unwrap();
+
+        InputWindow {
+            input: None,
+            size: (size.0 / 2, size.1 / 2),
+            pos: (size.0 / 4, size.1 / 2),
+            title: String::from("Input Window"),
+            scroll_x: 0,
+            content: String::new(),
+        }
+    }
+
+    fn add_char(&mut self, new_char: char) {
+        self.content.push(new_char);
+    }
+
+    fn remove_last_char(&mut self) {
+        if self.content.len() == 0 { return };
+        self.content = self.content[0..self.content.chars().count() - 1].to_string();
+    }
+
+    fn move_cursor_left(&mut self) {
+        // ...
+    }
+
+    fn move_cursor_right(&mut self) {
+        // ...
+    }
+
+    fn render(&self) {
+        let mut line = String::new();
+
+        for _ in 0..self.size.0 {
+            line.push('─')
+        }
+
+        println!("{}", termion::cursor::Goto(self.pos.0, self.pos.1));
+        println!("{}{}", termion::cursor::Up(1), self.title);
+        println!("{}", termion::cursor::Goto(self.pos.0, self.pos.1));
+        println!("╭{}╮", line);
+        println!("{}", termion::cursor::Goto(self.pos.0, self.pos.1 + 2));
+        println!("╰{}╯", line);
+        println!("{}", termion::cursor::Goto(self.pos.0, self.pos.1 + 1));
+        println!("│");
+        println!("{}", termion::cursor::Goto(self.size.0 + self.pos.0 + 1, self.pos.1 + 1));
+        println!("│");
+        println!("{}", termion::cursor::Goto(self.pos.0 + 1, self.pos.1 + 1));
+
+        println!("{}", self.content);
+        println!("{}", termion::cursor::Goto(
+                self.pos.0 + self.content.len() as u16 + 1,
+                self.pos.1 + 1));
+    }
+
+    fn set_size(&mut self, width: u16, height: u16) {
+        self.size = (width, height);
+    }
+
+    fn set_position(&mut self, x: u16, y: u16) {
+        self.pos = (x, y);
+    }
+
+    fn set_title(&mut self, title: String) {
+        self.title = title;
+    }
+
+    fn set_content(&mut self, content: String) {
+        self.content = content;
     }
 }
